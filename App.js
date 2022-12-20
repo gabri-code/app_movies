22aed4893c158c9428ed010b57fd5fb2bdf84eab
Gabriel Ferreira <gabriellf.14@hotmail.com>
2022-12-20 11:36:19 -0300

    feat: add activeindicator

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, ActivityIndicator } from 'react-native';
 import MovieCard from './src/components/MovieCard';
 import { api } from './src/services/api';
 
@@ -7,16 +7,27 @@ const MOVIES_ROUTE = '/r-api/?api=filmes';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getMovies = async () => {
       const response = await api.get(MOVIES_ROUTE);
 
       setMovies(response.data);
+
+      setIsLoading(false);
     };
 
     getMovies();
   }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -31,6 +42,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
 });
